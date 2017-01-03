1f0f23383894802b2c9bb167b63705aa862ee2c3
Vladimir Bochkov <vlbochk@mera.ru>
2017-01-03 20:43:55 +0300

    rebased solution from bluetooth oriented to wifi

--- README.md
+++ README.md
@@ -9,7 +9,12 @@ programm will receive commands from bluetooth and executes CVKernel and push com
 Key libraries:
 
 Pexpect - enabling bluetooth in start of application
-PyBluez - receiving commands from Android device via bluetooth
+
+PyBluez - receiving commands from Android device via bluetooth (wrong, Android doesn't support bluez stack)
+instead this just use sockets upon wifi connection to nexus hotspot
+
 os      - call fork and exec with CVKernel
+
 sys     - command line args
+
 serial  - send commands to arduino via Serial to USB
--- driver.py
+++ driver.py
@@ -1,25 +1,56 @@
-import pexpect
-import bluetooth
 import os
 import sys
 import serial
 import time
+import socket
+import netifaces
 
+
+
+def connect_to_server():
+    sock = socket.socket()
+    self_addr = netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
+    serv_addr = self_addr[:self_addr.rfind('.')] + '1'
+    sock.connect(serv_addr, 8989)
+    return sock
+
+def disconnect():
+    return
 
 def main():
-    exp = pexpect.spawn('bluetoothctl')
-    time.sleep(1)
-    exp.expect('#')
-    exp.sendline('power on')
-    time.sleep(2)
-    exp.expect('#')
-    exp.sendline('agent on')
-    time.sleep(3)
-    exp.expect('#')
-    exp.send('quit')
-    time.sleep(1)
-    exp.eof()
+    sock = connect_to_server()
+    while 1:
+        com = sock.recv(1)
+        if com is '0':
+            # activate autogun
+        elif com is '1':
+            # moving forward
+        elif com is '2':
+            # moving backward
+        elif com is '3':
+            # rotate robot right
+        elif com is '4':
+            # rotate robot left
+        elif com is '5':
+            # gun up
+        elif com is '6':
+            # gun down
+        elif com is '7':
+            # gun left
+        elif com is '8':
+            # gun right
+        elif com is 'p':
+            # pump on
+        elif com is 's':
+            # stop
 
+        if com is not '0':
+            # stop autogun
+
+        if com is 'e':
+            break
+
+    disconnect()
     return
 
 if __name__ == '__main__':
